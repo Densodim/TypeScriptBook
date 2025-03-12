b61c8fab8ee8f904e8aa5a24fdf62aa3dde18561
Densodim <25denso@gmail.com>
2025-03-13 00:14:24 +0300

    add chapter 11

--- src/11-Chapter/11.2.ts
+++ src/11-Chapter/11.2.ts
@@ -41,9 +41,9 @@ const canvas = document.getElementsByTagName('canvas')[0]
 const ctx = canvas?.getContext('2d');
 
 const shape = new ShapeClass([
-    {x:50, y:50},
-    {x:150, y:150},
-    {x:250, y:250},
+    {x: 50, y: 50},
+    {x: 150, y: 150},
+    {x: 250, y: 250},
 ]);
 
 shape.fill = 'red';
@@ -53,3 +53,31 @@ if (ctx) {
 } else {
     console.error("Canvas context is not available.");
 }
+
+class RectangleClass extends ShapeClass {
+    constructor(points: Point[]) {
+        if (points.length !== 2) {
+            throw new Error(`Canvas context is not available. got ${points.length}`);
+        }
+        super(points);
+    }
+
+    override render(ctx: CanvasRenderingContext2D) {
+        ctx.fillStyle = this.fillStyle;
+        ctx.lineWidth = this.lineWidth;
+        let a = this.points[0];
+        let b = this.points[1];
+        ctx.strokeRect(a.x, a.y, b.x - a.x, b.y - a.y)
+    }
+}
+
+const rectangle = new RectangleClass([
+    {x: 130, y: 190},
+    {x: 170, y: 250},
+])
+if (ctx) {
+    rectangle.render(ctx)
+} else {
+    console.error("Canvas context is not available.");
+}
+
